37c80d6505d381d4ab3e06809ec1b5e08fefe47d
Arunkambrekar <arunkabrekar@gmail.com>
2026-02-23 11:26:08 +0530

    Fix CI headless chrome isue

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -2,11 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 def get_driver():
     chrome_options = Options()
 
-    # Disable password manager popups
+    # Run headless in CI environment
+    if os.getenv("CI"):  
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Disable password popups
     chrome_prefs = {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False,
@@ -14,18 +21,9 @@ def get_driver():
     }
     chrome_options.add_experimental_option("prefs", chrome_prefs)
 
-    # Disable notifications & info bars
     chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--disable-extensions")
-
-    # Start maximized
     chrome_options.add_argument("--start-maximized")
 
-    # Optional (stability for automation)
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-
-    # Use WebDriver Manager (auto manages chromedriver)
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
